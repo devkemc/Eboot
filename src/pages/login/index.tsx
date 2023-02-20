d22705b8bf27d080584fb1cfc5dcb8d8d15dcbff
devkemc <joaquimkbs@gmail.com>
2023-02-20 13:24:32 -0300

    feat: layout login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,24 +1,29 @@
 import { Button, Container, Form } from "react-bootstrap";
-
+import { useNavigate } from "react-router-dom";
+import styles from "./styles.module.scss";
 export const Login = () => {
+  const navigate = useNavigate();
+
+  function handleSubmit() {
+    navigate("/");
+  }
   return (
-    <Container>
-      <Form>
+    <Container fluid className={styles.container}>
+      <Form onSubmit={handleSubmit}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">We'll never share your email with anyone else.</Form.Text>
+          <Form.Label>Login</Form.Label>
+          <Form.Control type="text" placeholder="entre com seu login" />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Label>Senha</Form.Label>
+          <Form.Control type="password" placeholder="entre com sua senha" />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
+          <Form.Check type="checkbox" label="mantenha-me conectado" />
         </Form.Group>
         <Button variant="primary" type="submit">
-          Submit
+          Entrar
         </Button>
       </Form>
     </Container>
