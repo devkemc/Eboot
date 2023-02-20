fe5d587b369e0e8cb1debf8772176e1167e83641
devkemc <joaquimkbs@gmail.com>
2023-02-20 19:56:42 -0300

    fix: suportando primeiro acesso

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -3,14 +3,17 @@ import { Link, useNavigate } from "react-router-dom";
 export const Login = () => {
   const navigate = useNavigate();
 
-  function handleSubmit() {
+  function handleSubmitLogin() {
     navigate("/");
+  }
+  function handleSubmitFirstAccess() {
+    navigate("/cadastrar-cliente");
   }
   return (
     <Container fluid className="d-flex justify-content-center align-items-center vh-100">
       <Row className="w-100 gap-5 d-flex justify-content-center">
-        <Col xxl={5} sm={12}>
-          <Form onSubmit={handleSubmit}>
+        <Col xs={12} sm={5}>
+          <Form onSubmit={handleSubmitLogin}>
             <h2>Já Sou cliente</h2>
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Login</Form.Label>
@@ -29,12 +32,12 @@ export const Login = () => {
             </Button>
           </Form>
         </Col>
-        <Col xxl={5} sm={12}>
-          <Form onSubmit={handleSubmit}>
+        <Col xs={12} sm={5}>
+          <Form onSubmit={handleSubmitFirstAccess}>
             <h2>Criar conta</h2>
             <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>CPF</Form.Label>
-              <Form.Control type="text" placeholder="entre com seu CPF" />
+              <Form.Label>email</Form.Label>
+              <Form.Control type="email" placeholder="digite seu email" />
             </Form.Group>
             <Button variant="primary" type="submit" className="w-100">
               Proseguir
