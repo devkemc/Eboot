a73e576280dd13fda8e65e81017f592a51945089
devkemc <joaquimkbs@gmail.com>
2023-02-25 20:33:44 -0300

    feat: adicionando rota para detalhes produtos

--- src/Router/index.tsx
+++ src/Router/index.tsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from "react-router-dom";
+import { ProductDetails } from "../components/product-details";
 import { DefaultLayout } from "../layouts/default-layout";
 import { CadastroCliente } from "../pages/cadastro-cliente";
 import { Home } from "../pages/home";
@@ -11,6 +12,7 @@ export const Router = () => {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/cadastrar-cliente" element={<CadastroCliente />} />
+        <Route path="/detalhes-produto" element={<ProductDetails />} />
       </Route>
     </Routes>
   );
