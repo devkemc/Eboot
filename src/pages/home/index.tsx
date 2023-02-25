3277eacaae42e08dbda231431391f24c359705d5
devkemc <joaquimkbs@gmail.com>
2023-02-25 20:32:40 -0300

    feat: pagina home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,8 +1,62 @@
-import { Button, Container } from "react-bootstrap";
-import { Header } from "../../components/header";
+import { Carousel, Col, Container, Image, Row } from "react-bootstrap";
+import img1 from "../../assets/Promocao.webp";
+import { Product } from "../../components/product";
 
 export const Home = () => {
   return (
-   <Header/>
-  )
+    <Container fluid=" xxl" className="vh-100 mt-5">
+      <Carousel>
+        <Carousel.Item>
+          <Image fluid style={{ minHeight: "40vh" }} src={img1} alt="First slide" />
+        </Carousel.Item>
+        <Carousel.Item>
+          <Image fluid style={{ minHeight: "40vh" }} src={img1} alt="Third slide" />
+        </Carousel.Item>
+      </Carousel>
+      <Container>
+        <Row>
+          <Col>
+            <Product />
+          </Col>
+          <Col>
+            <Product />
+          </Col>
+          <Col>
+            <Product />
+          </Col>
+          <Col>
+            <Product />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Product />
+          </Col>
+          <Col>
+            <Product />
+          </Col>
+          <Col>
+            <Product />
+          </Col>
+          <Col>
+            <Product />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Product />
+          </Col>
+          <Col>
+            <Product />
+          </Col>
+          <Col>
+            <Product />
+          </Col>
+          <Col>
+            <Product />
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
 };
